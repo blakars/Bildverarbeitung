726885dc38905954fd592940dfcbd4480b688b0a
DESKTOP-131RM2K\kabla <karsten.blank@posteo.de>
2022-04-24 11:05:23 +0200

    added comments

--- a2/a2.py
+++ a2/a2.py
@@ -22,32 +22,45 @@ def loadPicture():
     img = cv.imread(imgLoad)
     return img   
 
+#Main
 img = loadPicture()
-
 cv.namedWindow("Window")
 cv.setMouseCallback("Window",onClick)
 
 while True:
     cv.imshow("Window",img)
-
     key = cv.waitKey(1)
 
+    #wenn "q" -> break
     if key == 113:
         break
     
+    #wenn "n" -> neues Bild laden
     elif key == 110:
         img = loadPicture()
     
+    #wenn "s" -> Alpha-Kanal hinzufügen und freigestelltes Bild abspeichern
     elif key == 115:
-        alphablank = np.zeros((img.shape[0],img.shape[1]), dtype='uint8')
-        cv.fillPoly(alphablank,np.array([polygon], 'int32'),255)
-        #cv.imshow("xyz",alphablank)
-        imgRGBA=cv.cvtColor(img,cv.COLOR_RGB2RGBA)
-        #cv.imshow("RGBA",imgRGBA)
-        imgRGBA[:,:,3]=alphablank
-        #cv.imshow("RGBAadjusted",imgRGBA)
-        cv.imwrite('ausgeschnittenRGBA'+str(counter)+'.png',imgRGBA)
-        counter+=1
-        polygon=[]
+        if polygon ==[]:
+            print("Zunächst Polygon per Mausklick definieren")
+
+        else:
+            #Erstelle 0-Array für Alphakanal von Größe img-shape
+            alphablank = np.zeros((img.shape[0],img.shape[1]), dtype='uint8')
+
+            #Überschreibe polygon-Pixel des Alphakanal-Array mit "255"=transparent 
+            cv.fillPoly(alphablank,np.array([polygon]),255)
+
+            #Füge Alphakanal hinzu
+            imgRGBA=cv.cvtColor(img,cv.COLOR_RGB2RGBA)
+            
+            #Setze Alphakanal auf das oben erstellte "Transparenz-Array"
+            imgRGBA[:,:,3]=alphablank
+
+            #Bild anzeigen, abspeichern, Counter für Datei-Bezeichnung erhöhen und Polygon-Array leeren
+            cv.imshow("Ergebnis",imgRGBA)
+            cv.imwrite('RGBA'+str(counter)+'.png',imgRGBA)
+            counter+=1
+            polygon=[]
 
 cv.destroyAllWindows()
