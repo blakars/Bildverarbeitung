081aff5c11d54694843c8a5898947206e78dae56
DESKTOP-131RM2K\kabla <karsten.blank@posteo.de>
2022-05-15 11:08:37 +0200

    update a3 mit mouse hover

--- a3/Blank_Karsten_90294_Aufgabe_3.py
+++ a3/Blank_Karsten_90294_Aufgabe_3.py
@@ -5,11 +5,43 @@ from tkinter import filedialog
 #Variablen definieren (Polygon-Array und Zähler-Variable)
 coordinates=[]
 
-#Funktion für Mausklick
-def onClick(event,x,y,flags,param):
-    if event == cv.EVENT_LBUTTONDOWN:
-        coordinates.append((x,y))
-        cv.circle(img1,(x,y),4,(0,0,255),-1)
+#Funktion für Mauskmove und -klick
+def onMove(event,x,y,flags,param):
+    if event == cv.EVENT_MOUSEMOVE:
+        img1C=img1.copy()
+        if rows % 2 ==0 and cols % 2 ==0:
+            roi=img1C[y-rows//2:y+rows//2,x-cols//2:x+cols//2]
+        elif rows %2 == 0 and cols % 2!=0:
+            roi=img1C[y-rows//2:y+rows//2,x-cols//2-1:x+cols//2]
+        elif rows %2 != 0 and cols % 2 ==0:
+            roi=img1C[y-rows//2-1:y+rows//2,x-cols//2:x+cols//2]
+        else:
+            roi=img1C[y-rows//2-1:y+rows//2,x-cols//2-1:x+cols//2]
+
+        mask=img2[...,3]
+        mask_inverted=cv.bitwise_not(mask)
+        img1_bg=cv.bitwise_and(roi,roi,mask=mask_inverted)
+        img2_fg=cv.bitwise_and(img2,img2,mask=mask)
+
+        new=cv.add(img1_bg,img2_fg)
+
+        if rows % 2 ==0 and cols % 2 ==0:
+            img1C[y-rows//2:y+rows//2,x-cols//2:x+cols//2]=new
+        elif rows %2 == 0 and cols % 2!=0:
+            img1C[y-rows//2:y+rows//2,x-cols//2-1:x+cols//2]=new
+        elif rows %2 != 0 and cols % 2 ==0:
+            img1C[y-rows//2-1:y+rows//2,x-cols//2:x+cols//2]=new
+        else:
+            img1C[y-rows//2-1:y+rows//2,x-cols//2-1:x+cols//2]=new
+
+        #cv.circle(imgCopy,(x,y),4,(0,0,255),-1)
+        #roiGray=cv.cvtColor(roi, cv.COLOR_RGBA2GRAY)
+        #img2Gray=cv.cvtColor(img2,cv.COLOR_RGBA2GRAY)
+        res = cv.matchTemplate(roi,img2,cv.TM_SQDIFF_NORMED)
+        min_val,max_val,min_loc,max_loc=cv.minMaxLoc(res)
+        org = (30,30)
+        img1C = cv.putText(img1C,str(res),org,cv.FONT_HERSHEY_COMPLEX,1,(0,0,255),1,cv.LINE_AA)
+        cv.imshow("Window",img1C)
 
 #Funktion zum Laden eines Bildes
 def loadPicture():
@@ -20,18 +52,20 @@ def loadPicture():
 #Main
 img1 = loadPicture()
 cv.namedWindow("Window")
-
+img2=cv.imread('RGBA1.png',flags=cv.IMREAD_UNCHANGED)
+rows,cols,channels=img2.shape
 
 while True:
-    cv.setMouseCallback("Window",onClick)
     cv.imshow("Window",img1)
-    key = cv.waitKey(1)
+    cv.setMouseCallback("Window",onMove)
+    key = cv.waitKey(0)
 
     #wenn "q" -> break
     if key == 113:
         break
     
     #wenn "s" -> Alpha-Kanal hinzufügen und freigestelltes Bild abspeichern
+    '''
     elif key == 115:
         img2=cv.imread('RGBA1.png',flags=cv.IMREAD_UNCHANGED)
         rows,cols,channels=img2.shape
@@ -46,7 +80,7 @@ while True:
         else:
             roi=img1[y1-rows//2-1:y1+rows//2,x1-cols//2-1:x1+cols//2]
         
-        res = cv.matchTemplate(roi,img2,cv.TM_SQDIFF)
+        res = cv.matchTemplate(roi,img2,cv.TM_SQDIFF,mask=img2[...,3])
         #cv.normalize(res,res,0,1,cv.NORM_MINMAX,-1)
         min_val,max_val,min_loc,max_loc=cv.minMaxLoc(res)
 
@@ -71,6 +105,7 @@ while True:
             img1[y1-rows//2-1:y1+rows//2,x1-cols//2-1:x1+cols//2]=new
 
         org = (30,30)
-        img1 = cv.putText(img1,str(min_val),org,cv.FONT_HERSHEY_COMPLEX,1,(0,0,255),1,cv.LINE_AA)
+        img1 = cv.putText(img1,str(max_val),org,cv.FONT_HERSHEY_COMPLEX,1,(0,0,255),1,cv.LINE_AA)
+        '''
 
 cv.destroyAllWindows()
